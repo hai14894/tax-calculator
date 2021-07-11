5486a2370569bf65e1162a18a4559f6901010f2a
hai14894 <hohoanghai14894@gmail.com>
2021-07-11 15:48:22 +1000

    conditionally render Card's elements

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,21 +1,25 @@
 import React from "react";
-import "../style.css"
-import CardRectangle from "./CardRectangle"
-import CardText from "./CardText"
+import "../style.css";
+import CardRectangle from "./CardRectangle";
+import CardText from "./CardText";
 const Card = (props) => {
-    return (
-        <div className = "card">
-            <CardText text="Your estimated income tax payable is:"/>
-            
-            <CardRectangle>{props.income}</CardRectangle>
-            <CardText text="Breakdown"/>
-            <CardRectangle></CardRectangle>
-            <CardRectangle></CardRectangle>
-            <CardRectangle></CardRectangle>
-            <CardRectangle></CardRectangle>
-            <CardRectangle></CardRectangle>
+  const path = window.location.pathname;
+  return (
+    <div className="card">
+      {path !== "/" && (
+        <div>
+          <CardText text="Your estimated income tax payable is:" />
+          <CardRectangle>{props.income}</CardRectangle>
+          <CardText text="Breakdown" />
+          <CardRectangle></CardRectangle>
+          <CardRectangle></CardRectangle>
+          <CardRectangle></CardRectangle>
+          <CardRectangle></CardRectangle>
+          <CardRectangle></CardRectangle>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Card
+export default Card;
